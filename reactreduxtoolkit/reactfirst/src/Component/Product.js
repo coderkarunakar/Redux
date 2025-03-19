576de96f064ca3c265f99bc0efbbf9e31aed90e7
Karunakar <114292518+coderkarunakar@users.noreply.github.com>
2025-03-19 07:01:55 +0530

    integrated with fake api

--- reactreduxtoolkit/reactfirst/src/Component/Product.js
+++ reactreduxtoolkit/reactfirst/src/Component/Product.js
@@ -1,9 +1,17 @@
 import React from "react";
-
+import { useState,useEffect } from "react";
 const Product = () =>{
+
+    const [products, getProducts] = useState([]);
+    useEffect(()=>{
+       fetch('https://fakestoreapi.com/products')
+       .then(data => data.json())
+       .then(result => getProducts(result))
+    },[]);
     return(
         <>
         <h1>Product DashBoard</h1>
+        {JSON.stringify(products)}
         </>
     )
 }
