973ac57f8b0092e5de4c18c75aa42af6a3d69a16
Karunakar <114292518+coderkarunakar@users.noreply.github.com>
2025-03-24 08:15:54 +0530

    created a nav bar from react bootstrap
    using react bootstrap code i had created a nav bar

--- reactreduxtoolkit/reactfirst/src/Component/RootLayout.js
+++ reactreduxtoolkit/reactfirst/src/Component/RootLayout.js
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom";
 import React  from "react";
+import NavBar from "./NavBarPanel";
 const RootLayout = () =>{
     //using outlet we can render the child routes inside a parent route.
     return(
         <>
-        <h1>Navigation</h1>
+        <NavBar/>
         <main>
         
             <Outlet/>
