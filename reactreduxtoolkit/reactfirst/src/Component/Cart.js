35de786c0094491f1c0ab799b6d65fdc9f11227c
Karunakar <114292518+coderkarunakar@users.noreply.github.com>
2025-03-26 13:23:17 +0530

    dispalyed selected items in cart
    displayed all the selected items in the cart using useSelector hook in the cart.js

--- reactreduxtoolkit/reactfirst/src/Component/Cart.js
+++ reactreduxtoolkit/reactfirst/src/Component/Cart.js
@@ -1,8 +1,37 @@
+import { useSelector } from 'react-redux';
 import React from 'react';
+import { Card } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 const Cart =()=>{
+    const products = useSelector(state => state.cart);
+    const  cards = products.map(product => (
+        <div className="col-md-12" style={{marginbottom:'10px'}}>
+             <Card key={product.id}  className = "h-100" style={{ width: '18rem' }}>
+        
+                <div>
+                <Card.Img variant="top" src= {product.image} style={{width : '100px', height: '130px'}} />
+                </div>
+                <Card.Body>
+                    <Card.Title>{product.title}</Card.Title>
+                    <Card.Text>
+                        INR.{product.price}
+                    </Card.Text>
+                    
+                
+                </Card.Body>
+                <Card.Footer style={{background : 'white'}}>
+                <Button variant="primary" >Add to Cart</Button>
+                </Card.Footer>
+            </Card>
+        </div>
+    ))
     return(
         <>
          <h1>Cart</h1>
+         <div className='row'>
+
+            {cards}
+         </div>
         </>
        
     )
