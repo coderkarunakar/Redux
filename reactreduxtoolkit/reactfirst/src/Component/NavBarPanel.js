7a5fb2e1839c071ac123626cd3a07022c0fd0b08
Karunakar <114292518+coderkarunakar@users.noreply.github.com>
2025-03-26 12:56:16 +0530

    implemented use selector hook
    with the help of useselector hook we can read , and with the help of usedispatch we  can perform add operation

--- reactreduxtoolkit/reactfirst/src/Component/NavBarPanel.js
+++ reactreduxtoolkit/reactfirst/src/Component/NavBarPanel.js
@@ -6,8 +6,13 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const NavBar = () => {
+
+  //note with the help of use selector hook we can read in the redux
+  //this will pick the cart(key) which is inside the store
+  const cartProducts = useSelector(state => state.cart);
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
           <Container fluid>
@@ -22,7 +27,7 @@ const NavBar = () => {
 
               <Navbar.Collapse className = 'justify-content-end'>
                 <Navbar.Text>
-                    <Nav.Link to = "/cart" as={Link}> My Bag 0 </Nav.Link>
+                    <Nav.Link to = "/cart" as={Link}> My Bag {cartProducts.length}</Nav.Link>
                 </Navbar.Text>
               </Navbar.Collapse>
               
