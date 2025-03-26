382a9f48657ae78a149bb51d008f688d19714909
Karunakar <114292518+coderkarunakar@users.noreply.github.com>
2025-03-26 15:13:56 +0530

    removed items using dispatch
    using dispatch method we can remove the items as well

--- reactreduxtoolkit/reactfirst/src/Component/Cart.js
+++ reactreduxtoolkit/reactfirst/src/Component/Cart.js
@@ -1,9 +1,16 @@
-import { useSelector } from 'react-redux';
+import { useSelector ,useDispatch} from 'react-redux';
 import React from 'react';
 import { Card } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
+import { remove } from '../store/cartSlice';
 const Cart =()=>{
     const products = useSelector(state => state.cart);
+    const dispatch = useDispatch()
+    //ultimatelly we will get this id from the products since see below i am using product and iterating and storing everything there
+    const removeToCart = (id) =>{
+        //dispatch the remove action
+        dispatch(remove(id))
+    }
     const  cards = products.map(product => (
         <div className="col-md-12" style={{marginbottom:'10px'}}>
              <Card key={product.id}  className = "h-100" style={{ width: '18rem' }}>
@@ -20,7 +27,7 @@ const Cart =()=>{
                 
                 </Card.Body>
                 <Card.Footer style={{background : 'white'}}>
-                <Button variant="primary" >Add to Cart</Button>
+                <Button variant="danger" onClick={()=> removeToCart(product.id)}>Remove Item</Button>
                 </Card.Footer>
             </Card>
         </div>
--- reactreduxtoolkit/reactfirst/src/store/cartSlice.js
+++ reactreduxtoolkit/reactfirst/src/store/cartSlice.js
@@ -8,12 +8,16 @@ const cartSlice = createSlice({
         add(state, action){
             //what ever the data i will get i will update and push to state
             state.push(action.payload)
+        },
+        remove(state,action){
+            //filter based on id,keeps all items except that one id which mathces with the action .payload
+            return state.filter(item => item.id != action.payload)
         }
     }
 });
 
 //slice is a collection of redux reduce the logic ,actions for a single feature
 //action will give you the functions
-export const {add} = cartSlice.actions;
+export const {add,remove} = cartSlice.actions;
 //here reducer will give you the state
 export default cartSlice.reducer;
